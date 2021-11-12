5d02d194d426ab922444c915a7daa7c30b987f68
aleksandr <60693889+shabanov3552@users.noreply.github.com>
2021-11-13 02:43:02 +0700

    Delete #src directory

--- #src/js/app.js
+++
@@ -1,5 +0,0 @@
-@@include('files/functions.js', {})
-@@include('files/forms.js', {})
-@@include('files/dynamic_adapt.js', {})
-@@include('files/sliders.js', {})
-@@include('files/script.js', {})
--- #src/js/vendors.js
+++
@@ -1,2 +0,0 @@
-@@include('libs/swiper.min.js', {})
-@@include('libs/jquery.nice-select.min.js', {})
